bfa7384146a0c4f03f923677ccad2839968a6606
Shichao An <shichao.an@nyu.edu>
2015-09-16 17:03:21 -0700

    v0.1.2 and resolved #4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='homura',
-    version='0.1.1',
+    version='0.1.2',
     description="Python downloader with progress",
     long_description=open('README.rst').read(),
     keywords='download progress',
